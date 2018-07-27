9eaebf169d51cadbe86ab4a5d907772d3c6a079f
Bagus Nurtomo <bagusthanatos@gmail.com>
2018-07-27 09:04:53 +0700

    fix linux build and remove node.h include

--- linux_build.sh
+++ linux_build.sh
@@ -1,2 +1,2 @@
-mkdir -p ../build/linux/
-clang src/main.cpp -O3 -o ../build/linux/main
+mkdir -p build/linux/
+clang src/main.cpp -O3 -o build/linux/main
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include "node.h"
-
 
 int main(int argc, char *argv[], char *envp[]){
     printf("bagus");
